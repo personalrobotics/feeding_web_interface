328a09d81938b63c176cec5b2e9108f5f59785f8
a380ask <a380ask@outlook.com>
2022-09-05 22:59:43 -0700

    minor edits

--- feedingwebapp/src/Pages/Settings/Settings.js
+++ feedingwebapp/src/Pages/Settings/Settings.js
@@ -1,16 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
-import Footer from "../Footer/Footer";
-
-import { useROS } from 'react-ros'
 
 const Settings = () => {
     return (
-        
         <div>
             <style type="text/css">
                 {`
@@ -35,44 +31,43 @@ const Settings = () => {
             <h1 style={{ textAlign: "center", "font-size": "40px" }} className='txt-huge'>⚙ Settings</h1>
 
             <Row className="justify-content-center mx-1 my-2" >
-                <Form.Label style={{fontSize: "30px"}}>Your preferred feeding style: </Form.Label>
+                <Form.Label style={{ fontSize: "30px" }}>Your preferred feeding style: </Form.Label>
                 <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-2" value={2}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-2" value={2}>
                         Side Feeding
                     </ToggleButton>
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-3" value={3}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-3" value={3}>
                         Front Feeding
                     </ToggleButton>
                 </ToggleButtonGroup>
             </Row>
 
             <Row className="justify-content-center mx-1 my-2">
-                <Form.Label style={{fontSize: "30px"}}>Your preferred method of indicating readiness for a bite: </Form.Label>
+                <Form.Label style={{ fontSize: "30px" }}>Your preferred method of indicating readiness for a bite: </Form.Label>
                 <ToggleButtonGroup type="radio" name="options2" defaultValue={1} >
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-4" value={2}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-4" value={2}>
                         Open Mouth
                     </ToggleButton>
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-5" value={3}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-5" value={3}>
                         Say "I'm done"
                     </ToggleButton>
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-6" value={3}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-6" value={3}>
                         Press button
                     </ToggleButton>
                 </ToggleButtonGroup>
             </Row>
 
-            <Row className="justify-content-center mx-1 my-2" style={{ paddingBottom: '35vh'}}>
-                <Form.Label style={{fontSize: "30px"}}>Your preferred method for indicating selection of food: </Form.Label>
+            <Row className="justify-content-center mx-1 my-2" style={{ paddingBottom: '35vh' }}>
+                <Form.Label style={{ fontSize: "30px" }}>Your preferred method for indicating selection of food: </Form.Label>
                 <ToggleButtonGroup type="radio" name="options2" defaultValue={1} >
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-4" value={2}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-4" value={2}>
                         A picture of plate
                     </ToggleButton>
-                    <ToggleButton className="border border-white btn-huge" style={{paddingLeft: '0px', paddingRight: '0px'}} id="tbg-radio-5" value={3}>
+                    <ToggleButton className="border border-white btn-huge" style={{ paddingLeft: '0px', paddingRight: '0px' }} id="tbg-radio-5" value={3}>
                         Scrollable list of food
                     </ToggleButton>
                 </ToggleButtonGroup>
             </Row>
-
         </div>
     );
 }
