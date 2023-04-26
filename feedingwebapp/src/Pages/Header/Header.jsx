477782beb88eefbc365f8be0feb1694cfabaee5d
taylorkf <taylorkf@uw.edu>
2023-04-25 17:05:35 -0700

    Removed connection text and reduced header font size

--- feedingwebapp/src/Pages/Header/Header.jsx
+++ feedingwebapp/src/Pages/Header/Header.jsx
@@ -58,10 +58,11 @@ const Header = () => {
        * The ToastContainer is an alert that pops up on the top of the screen
        * and has a timeout.
        */}
-      <ToastContainer style={{ fontSize: '28px' }} />
+      <ToastContainer style={{ fontSize: '24px' }} />
       {/**
        * The NavBar has two elements, Home and Settings, on the left side and one
-       * element, Video, on the right side.
+       * element, Video, on the right side. An image showing the connection status
+       * of the robot is placed in between Settings and Video.
        */}
       <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' sticky='top'>
         <Navbar id='responsive-navbar-nav'>
@@ -69,39 +70,37 @@ const Header = () => {
             <Nav.Link
               onClick={homeClicked}
               className='text-dark bg-info border border-info rounded mx-1 btn-lg btn-huge p-2'
-              style={{ fontSize: '175%' }}
+              style={{ fontSize: '150%' }}
             >
               Home
             </Nav.Link>
             <Nav.Link
               onClick={settingsClicked}
               className='text-dark bg-info border border-info rounded mx-1 btn-lg btn-huge p-2'
-              style={{ fontSize: '175%' }}
+              style={{ fontSize: '150%' }}
             >
               Settings
             </Nav.Link>
           </Nav>
-          {/**
-           * Show the connection status of the robot.
-           */}
           {isConnected ? (
             <div>
               <p className='connectedDiv' style={{ fontSize: '24px' }}>
-                🔌 connected
+                🔌
               </p>
             </div>
           ) : (
             <div>
               <p className='notConnectedDiv' style={{ fontSize: '24px' }}>
-                ⛔ not connected
+                ⛔
               </p>
             </div>
           )}
+          s
           <Nav>
             <Nav.Link
               onClick={() => setVideoShow(true)}
               className='text-dark bg-info border border-info rounded mx-1 btn-lg btn-huge p-2'
-              style={{ fontSize: '175%' }}
+              style={{ fontSize: '150%' }}
             >
               Video
             </Nav.Link>
