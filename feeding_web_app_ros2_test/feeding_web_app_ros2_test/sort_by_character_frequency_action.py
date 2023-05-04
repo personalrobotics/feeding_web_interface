e60735e9c32c691468386c4463bb213c880655f3
Amal Nanavati <amaln@uw.edu>
2023-05-04 13:30:14 -0700

    Formatted Python code using black

--- feeding_web_app_ros2_test/feeding_web_app_ros2_test/sort_by_character_frequency_action.py
+++ feeding_web_app_ros2_test/feeding_web_app_ros2_test/sort_by_character_frequency_action.py
@@ -9,32 +9,35 @@ from rclpy.node import Node
 import inspect
 from feeding_web_app_ros2_msgs.action import SortByCharacterFrequency
 
-class MinimalAction(Node):
 
+class MinimalAction(Node):
     def __init__(self, sleep_time=0.1):
-        super().__init__('sort_by_character_frequency')
+        super().__init__("sort_by_character_frequency")
         self.sleep_time = sleep_time
         self._action_server = ActionServer(
             self,
             SortByCharacterFrequency,
-            'sort_by_character_frequency',
+            "sort_by_character_frequency",
             self.execute_callback,
             goal_callback=self.goal_callback,
-            cancel_callback=self.cancel_callback)
+            cancel_callback=self.cancel_callback,
+        )
 
     def goal_callback(self, goal_request):
         """Accept or reject a client request to begin an action."""
         # This server allows multiple goals in parallel
-        self.get_logger().info('Received goal request')
+        self.get_logger().info("Received goal request")
         return GoalResponse.ACCEPT
 
     def cancel_callback(self, goal_handle):
         """Accept or reject a client request to cancel an action."""
-        self.get_logger().info('Received cancel request')
+        self.get_logger().info("Received cancel request")
         return CancelResponse.ACCEPT
 
     async def execute_callback(self, goal_handle):
-        self.get_logger().info('Executing goal...%s | %s' % (goal_handle, repr(goal_handle.request.input)))
+        self.get_logger().info(
+            "Executing goal...%s | %s" % (goal_handle, repr(goal_handle.request.input))
+        )
         input = goal_handle.request.input
 
         feedback_msg = SortByCharacterFrequency.Feedback()
@@ -43,7 +46,7 @@ class MinimalAction(Node):
         for i in range(len(input)):
             # Check if there is a cancel request
             if goal_handle.is_cancel_requested:
-                self.get_logger().info('Goal canceled')
+                self.get_logger().info("Goal canceled")
                 goal_handle.canceled()
                 return SortByCharacterFrequency.Result()
 
@@ -55,10 +58,10 @@ class MinimalAction(Node):
 
             # Update our progress
             feedback_msg.progress = float(i) / float(len(input))
-            self.get_logger().info('Feedback: %f' % feedback_msg.progress)
+            self.get_logger().info("Feedback: %f" % feedback_msg.progress)
             goal_handle.publish_feedback(feedback_msg)
 
-            # Sleep (to simulate the delay that a robot might have during a 
+            # Sleep (to simulate the delay that a robot might have during a
             # ROS action)
             time.sleep(self.sleep_time)
 
@@ -66,7 +69,9 @@ class MinimalAction(Node):
 
         result = SortByCharacterFrequency.Result()
         uniqueLetters = "".join(character_frequency.keys())
-        result.result = "".join(sorted(uniqueLetters, key=lambda x: character_frequency[x], reverse=True))
+        result.result = "".join(
+            sorted(uniqueLetters, key=lambda x: character_frequency[x], reverse=True)
+        )
         return result
 
 
@@ -81,5 +86,5 @@ def main(args=None):
     rclpy.spin(minimal_action, executor=executor)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- feeding_web_app_ros2_test/setup.py
+++ feeding_web_app_ros2_test/setup.py
@@ -1,29 +1,28 @@
 from setuptools import setup
 
-package_name = 'feeding_web_app_ros2_test'
+package_name = "feeding_web_app_ros2_test"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Amal Nanavati',
-    maintainer_email='amaln@cs.washington.edu',
-    description='A minimal ROS publisher, subscriber, service, and action to use to test the web app.',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Amal Nanavati",
+    maintainer_email="amaln@cs.washington.edu",
+    description="A minimal ROS publisher, subscriber, service, and action to use to test the web app.",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-                'listener = feeding_web_app_ros2_test.subscriber:main',
-                'reverse_string = feeding_web_app_ros2_test.reverse_string_service:main',
-                'sort_by_character_frequency = feeding_web_app_ros2_test.sort_by_character_frequency_action:main',
-                'talker = feeding_web_app_ros2_test.publisher:main',
+        "console_scripts": [
+            "listener = feeding_web_app_ros2_test.subscriber:main",
+            "reverse_string = feeding_web_app_ros2_test.reverse_string_service:main",
+            "sort_by_character_frequency = feeding_web_app_ros2_test.sort_by_character_frequency_action:main",
+            "talker = feeding_web_app_ros2_test.publisher:main",
         ],
-},
+    },
 )
