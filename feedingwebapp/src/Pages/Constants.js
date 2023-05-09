bf18eb6422a969588f44787de3342ce7fc65c4fe
Raida Karim <rk1997@uw.edu>
2023-05-08 22:31:05 -0700

    fix staging position icon

--- feedingwebapp/src/Pages/Constants.js
+++ feedingwebapp/src/Pages/Constants.js
@@ -13,6 +13,6 @@ export const REALSENSE_HEIGHT = 480
 let FOOTER_STATE_ICON_DICT = {}
 FOOTER_STATE_ICON_DICT[MEAL_STATE.R_MovingAbovePlate] = '/robot_state_imgs/move_above_plate_position.svg'
 FOOTER_STATE_ICON_DICT[MEAL_STATE.R_BiteAcquisition] = '/robot_state_imgs/move_to_bite_acquisition_position.svg'
-FOOTER_STATE_ICON_DICT[MEAL_STATE.R_MovingToStagingLocation] = '/robot_state_imgs/move_to_staging_position_footer.svg'
+FOOTER_STATE_ICON_DICT[MEAL_STATE.R_MovingToStagingLocation] = '/robot_state_imgs/move_to_staging_position.svg'
 FOOTER_STATE_ICON_DICT[MEAL_STATE.R_MovingToMouth] = '/robot_state_imgs/move_to_mouth_position.svg'
 export { FOOTER_STATE_ICON_DICT }
