b2ae1a4067c3ed2a1c90c715001f1cb85e70174b
Raida Karim <rk1997@uw.edu>
2023-05-24 01:39:08 +0000

    change bite done option

--- feedingwebapp/src/Pages/Home/MealStates/BiteDone.jsx
+++ feedingwebapp/src/Pages/Home/MealStates/BiteDone.jsx
@@ -11,15 +11,13 @@ import { FOOTER_STATE_ICON_DICT } from '../../Constants'
 /**
  * The BiteDone component appears after the robot has moved to the user's mouth,
  * and waits for the user to specify that they have finished the bite before
- * moving back to the staging area.
+ * moving back to above plate.
  */
 const BiteDone = () => {
   // Get the relevant global variables
   const setMealState = useGlobalState((state) => state.setMealState)
   // Get icon image for move above plate
   let moveAbovePlateImage = FOOTER_STATE_ICON_DICT[MEAL_STATE.R_MovingAbovePlate]
-  // Get icon image for move to mouth
-  let moveToMouthImage = FOOTER_STATE_ICON_DICT[MEAL_STATE.R_MovingToMouth]
 
   /**
    * Callback function for when the user wants to move above plate.
@@ -28,49 +26,25 @@ const BiteDone = () => {
     setMealState(MEAL_STATE.R_MovingAbovePlate)
   }, [setMealState])
 
-  /**
-   * Callback function for if the user decides to move to mouth position.
-   *
-   */
-  const moveToMouth = useCallback(() => {
-    setMealState(MEAL_STATE.R_MovingToMouth)
-  }, [setMealState])
-
   // Render the component
   return (
     <div style={{ display: 'block', width: '100%', height: '115vh', overflowX: 'hidden', overflowY: 'auto' }} className='outer'>
+      {/* Add empty space */}
+      <div className='justify-content-center mx-auto mt-5 row'>&nbsp;</div>
       {/* Ask the user whether they want to move to above plate position */}
       <p className='transitionMessage' style={{ marginBottom: '0px', fontSize: '140%' }}>
         Bite finished? Move above plate.
       </p>
       {/* Icon to move above plate */}
-      <Row className='justify-content-center mx-auto mb-2 w-75'>
+      <Row className='justify-content-center mx-auto mt-2 w-75'>
         <Button
           variant='success'
-          className='mx-2 mb-2 btn-huge'
+          className='mx-2 mt-2 btn-huge'
           size='lg'
           onClick={moveAbovePlate}
           style={{ width: '300px', height: '200px' }}
         >
           <img src={moveAbovePlateImage} alt='move_above_plate_image' className='center' />
-        </Button>
-      </Row>
-      {/* Add empty space */}
-      <div className='justify-content-center mx-auto my-3 row'>&nbsp;</div>
-      <Row className='justify-content-center mx-auto mt-2'>
-        {/* Ask the user whether they want to move to staging position */}
-        <p className='transitionMessage' style={{ marginBottom: '0px', fontSize: '140%' }}>
-          Take another bite? Move to mouth position.
-        </p>
-        {/* Icon to move to mouth position */}
-        <Button
-          variant='warning'
-          className='mx-2 mb-2 btn-huge'
-          size='lg'
-          onClick={moveToMouth}
-          style={{ width: '300px', height: '200px' }}
-        >
-          <img src={moveToMouthImage} alt='move_to_mouth_image' className='center' />
         </Button>
       </Row>
     </div>
