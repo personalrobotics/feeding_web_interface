4241f852769fdbd1684622d763c23df8b5fa920d
Amal Nanavati <amaln@uw.edu>
2024-01-25 16:40:14 -0800

    Run server in pm2 to restart on crash

--- feedingwebapp/server.js
+++ feedingwebapp/server.js
@@ -8,6 +8,7 @@
 
 const SegfaultHandler = require('segfault-handler')
 SegfaultHandler.registerHandler('crash.log')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -40,86 +41,86 @@ app.post('/subscribe', async ({ body }, res) => {
       ]
     })
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: created peer connection object")
+      console.log(Date(Date.now()).toString(), 'subscribe: created peer connection object')
     }
 
     // Close any old peers on the same IP address
     const topic = body.topic
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: got topic", topic)
+      console.log(Date(Date.now()).toString(), 'subscribe: got topic', topic)
     }
     const key = body.ip + ':' + topic
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: got key", key)
+      console.log(Date(Date.now()).toString(), 'subscribe: got key', key)
     }
     if (key in subscribePeers && subscribePeers[key] && subscribePeers[key].connectionState !== 'closed') {
       if (debug) {
-        console.log(Date(Date.now()).toString(), "subscribe: peer for key already exists")
+        console.log(Date(Date.now()).toString(), 'subscribe: peer for key already exists')
       }
       const senders = subscribePeers[key].getSenders()
       if (debug) {
-        console.log(Date(Date.now()).toString(), "subscribe: got senders for peer for key")
+        console.log(Date(Date.now()).toString(), 'subscribe: got senders for peer for key')
       }
       senders.forEach((sender) => subscribePeers[key].removeTrack(sender))
       if (debug) {
-        console.log(Date(Date.now()).toString(), "subscribe: removed tracks")
+        console.log(Date(Date.now()).toString(), 'subscribe: removed tracks')
       }
       subscribePeers[key].close()
       if (debug) {
-        console.log(Date(Date.now()).toString(), "subscribe: closed peer connection")
+        console.log(Date(Date.now()).toString(), 'subscribe: closed peer connection')
       }
     }
     subscribePeers[key] = peer
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: set new peer connection")
+      console.log(Date(Date.now()).toString(), 'subscribe: set new peer connection')
     }
 
     const desc = new webrtc.RTCSessionDescription(body.sdp)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: created desc")
+      console.log(Date(Date.now()).toString(), 'subscribe: created desc')
     }
     await peer.setRemoteDescription(desc)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: set remote desc")
+      console.log(Date(Date.now()).toString(), 'subscribe: set remote desc')
     }
 
     // Add the publisher's video stream to the subscriber's peer connection
     if (topic in senderStream) {
       if (debug) {
-        console.log(Date(Date.now()).toString(), "subscribe: adding topics from publisher")
+        console.log(Date(Date.now()).toString(), 'subscribe: adding topics from publisher')
       }
       senderStream[topic].getTracks().forEach((track) => peer.addTrack(track, senderStream[topic]))
       if (debug) {
-        console.log(Date(Date.now()).toString(), "subscribe: added topics from publisher")
+        console.log(Date(Date.now()).toString(), 'subscribe: added topics from publisher')
       }
     }
 
     // Create an answer to the publisher's offer
     const answer = await peer.createAnswer()
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: created answer")
+      console.log(Date(Date.now()).toString(), 'subscribe: created answer')
     }
     await peer.setLocalDescription(answer)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: set local description")
+      console.log(Date(Date.now()).toString(), 'subscribe: set local description')
     }
     const payload = {
-      sdp: answer
+      sdp: peer.localDescription
     }
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: created payload")
+      console.log(Date(Date.now()).toString(), 'subscribe: created payload')
     }
 
     // Send the answer to the publisher
     res.json(payload)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: set payload")
+      console.log(Date(Date.now()).toString(), 'subscribe: set payload')
     }
   } catch (err) {
     console.error(Date(Date.now()).toString(), 'subscribe: Failed to process subscriber, exception: ' + err.message)
     res.sendStatus(500)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "subscribe: sent error status 500")
+      console.log(Date(Date.now()).toString(), 'subscribe: sent error status 500')
     }
   }
 })
@@ -137,80 +138,80 @@ app.post('/publish', async ({ body }, res) => {
       ]
     })
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: create peer")
+      console.log(Date(Date.now()).toString(), 'publish: create peer')
     }
 
     // Close any old peers on the same IP address
     const topic = body.topic
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: got topic", topic)
+      console.log(Date(Date.now()).toString(), 'publish: got topic', topic)
     }
     const key = body.ip + ':' + topic
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: got key", key)
+      console.log(Date(Date.now()).toString(), 'publish: got key', key)
     }
     if (key in publishPeers && publishPeers[key] && publishPeers[key].connectionState !== 'closed') {
       if (debug) {
-        console.log(Date(Date.now()).toString(), "publish: found existing publisher for key", key)
+        console.log(Date(Date.now()).toString(), 'publish: found existing publisher for key', key)
       }
       const senders = publishPeers[key].getSenders()
       if (debug) {
-        console.log(Date(Date.now()).toString(), "publish: got senders for old key")
+        console.log(Date(Date.now()).toString(), 'publish: got senders for old key')
       }
       senders.forEach((sender) => publishPeers[key].removeTrack(sender))
       if (debug) {
-        console.log(Date(Date.now()).toString(), "publish: removed tracks for old key")
+        console.log(Date(Date.now()).toString(), 'publish: removed tracks for old key')
       }
       publishPeers[key].close()
       if (debug) {
-        console.log(Date(Date.now()).toString(), "publish: closed old peer connection")
+        console.log(Date(Date.now()).toString(), 'publish: closed old peer connection')
       }
     }
     publishPeers[key] = peer
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: added new peer")
+      console.log(Date(Date.now()).toString(), 'publish: added new peer')
     }
 
     // Send the publisher's video stream to all subscribers on that topic
     peer.ontrack = (e) => handleTrackEvent(e, topic)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: handled track events")
+      console.log(Date(Date.now()).toString(), 'publish: handled track events')
     }
 
     // Create an answer to the publisher's offer
     const desc = new webrtc.RTCSessionDescription(body.sdp)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: got desc")
+      console.log(Date(Date.now()).toString(), 'publish: got desc')
     }
     await peer.setRemoteDescription(desc)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: set remote description")
+      console.log(Date(Date.now()).toString(), 'publish: set remote description')
     }
     const answer = await peer.createAnswer()
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: got answer")
+      console.log(Date(Date.now()).toString(), 'publish: got answer')
     }
     await peer.setLocalDescription(answer)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: set local description")
+      console.log(Date(Date.now()).toString(), 'publish: set local description')
     }
     const payload = {
-      sdp: answer
+      sdp: peer.localDescription
     }
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: created payload")
+      console.log(Date(Date.now()).toString(), 'publish: created payload')
     }
 
     // Send the answer to the publisher
     res.json(payload)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: set json payload")
+      console.log(Date(Date.now()).toString(), 'publish: set json payload')
     }
   } catch (err) {
     console.error(Date(Date.now()).toString(), 'publish: Failed to process publisher, exception: ' + err.message)
     res.sendStatus(500)
     if (debug) {
-      console.log(Date(Date.now()).toString(), "publish: sent error status 500")
+      console.log(Date(Date.now()).toString(), 'publish: sent error status 500')
     }
   }
 })
