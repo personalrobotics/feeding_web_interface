af4cabd10a49636076ede2792030273a1fcd4411
Amal Nanavati <amaln@uw.edu>
2024-03-26 10:43:20 -0700

    Added a HoldButton to the app

---
+++ feedingwebapp/src/buttons/HoldButton.jsx
@@ -0,0 +1,71 @@
+// React imports
+import React, { useCallback, useEffect, useRef } from 'react'
+import Button from 'react-bootstrap/Button'
+// PropTypes is used to validate that the used props are in fact passed to this Component
+import PropTypes from 'prop-types'
+
+/**
+ * A button that responds to being held down. Specifically, as soon as the mouse
+ * first goes down, this button will start an interval at a user-specified rate
+ * that calls a user-specified function. When the mouse goes up or leaves the
+ * button, the interval is cleared and the component calls a user-specified
+ * cleanup function.
+ *
+ * @param {float} rate_hz - The rate at which to call the function, in Hz
+ * @param {function} holdCallback - The function to call at the specified rate
+ *    while the mouse is down. Note that this function must be static and cannot
+ *    use any variables outputts by a React hook (e.g. useState, useRef, etc.)
+ * @param {function} cleanupCallback - The function to call when the button stops
+ *    being held. Note that this function must be static and cannot use any
+ *    variables outputts by a React hook (e.g. useState, useRef, etc.)
+ * @param {Object} buttonStyle - Optional style for the button
+ *
+ */
+function HoldButton(props) {
+  // Reference to the interval
+  const intervalRef = useRef(null)
+
+  // Callback to stop the interval
+  const stopInterval = useCallback(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
+      let cleanupCallback = props.cleanupCallback
+      cleanupCallback()
+    }
+  }, [props.cleanupCallback])
+
+  // Callback to start the rate_hz interval
+  const startInterval = useCallback(() => {
+    // Stop the interval if it exists
+    stopInterval()
+    // Start a new interval
+    intervalRef.current = setInterval(() => {
+      let holdCallback = props.holdCallback
+      holdCallback()
+    }, 1000.0 / props.rate_hz)
+  }, [props.rate_hz, props.holdCallback, stopInterval])
+
+  // Stop the interval when the component is unmounted
+  useEffect(() => {
+    return stopInterval
+  }, [stopInterval])
+
+  return (
+    <Button style={props.buttonStyle} onMouseDown={startInterval} onMouseUp={stopInterval} onMouseLeave={stopInterval}>
+      {props.children}
+    </Button>
+  )
+}
+HoldButton.propTypes = {
+  rate_hz: PropTypes.number.isRequired,
+  holdCallback: PropTypes.func.isRequired,
+  cleanupCallback: PropTypes.func.isRequired,
+  buttonStyle: PropTypes.object,
+  children: PropTypes.node.isRequired
+}
+HoldButton.defaultProps = {
+  buttonStyle: {}
+}
+
+export default HoldButton
