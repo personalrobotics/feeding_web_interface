0d50ae2fdc93f0970b703225205cd97789514b7a
Jose Jaime <jose33jaime@gmail.com>
2025-01-13 17:06:10 -0800

    Update copyright date for 2025 (#151)
    Run pre-commit routine for 2025

--- feeding_web_app_ros2_test/feeding_web_app_ros2_test/reverse_string_service.py
+++ feeding_web_app_ros2_test/feeding_web_app_ros2_test/reverse_string_service.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2024, Personal Robotics Laboratory
+# Copyright (c) 2024-2025, Personal Robotics Laboratory
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
 
 from feeding_web_app_ros2_msgs.srv import ReverseString
--- feedingwebapp/start_robot_browser.js
+++ feedingwebapp/start_robot_browser.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * Copyright (c) 2024, Personal Robotics Laboratory
+ * Copyright (c) 2024-2025, Personal Robotics Laboratory
  * License: BSD 3-Clause. See LICENSE.md file in root directory.
  */
 
